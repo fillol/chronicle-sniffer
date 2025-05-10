08e3243ef5d07313eaa34d40ef0a6ebacff420f9
fillol <fillo.lucchesi@gmail.com>
2025-05-10 18:15:04 +0200

    validate spike

--- terraform/modules/cloudrun_processor/variables.tf
+++ terraform/modules/cloudrun_processor/variables.tf
@@ -28,20 +28,19 @@ variable "service_account_email" {
   description = "Email del Service Account da associare al servizio Cloud Run."
   type        = string
 }
-variable "env_vars" {
-  type    = map(string)
-  default = {}
-}
+
 variable "max_concurrency" {
   description = "Massima concorrenza per istanza."
   type        = number
   default     = 80 # Default di Cloud Run
 }
+
 variable "cpu_limit" {
   description = "Limite CPU per istanza."
   type        = string
   default     = "1000m"
 }
+
 variable "memory_limit" {
   description = "Limite memoria per istanza."
   type        = string
