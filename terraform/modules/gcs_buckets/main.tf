fc4f0ea88fa7a7bd04ef0e686f25e94264aeca49
fillol <fillo.lucchesi@gmail.com>
2025-05-16 20:12:45 +0200

    FUNZIONA

--- terraform/modules/gcs_buckets/main.tf
+++ terraform/modules/gcs_buckets/main.tf
@@ -29,7 +29,7 @@ resource "google_storage_bucket" "processed_udm" {
   project                     = var.project_id
   name                        = var.processed_udm_bucket_name
   location                    = var.location
-  force_destroy               = false
+  force_destroy               = true // siamo in fase di test, quindi possiamo perdere le catture
   uniform_bucket_level_access = true
 
   dynamic "versioning" {
