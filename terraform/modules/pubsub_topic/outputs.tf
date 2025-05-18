ab343c66e1da3583a0d8b78adf33be60f0503fca
fillol <fillo.lucchesi@gmail.com>
2025-05-18 19:20:20 +0200

    delta e

--- terraform/modules/pubsub_topic/outputs.tf
+++ terraform/modules/pubsub_topic/outputs.tf
@@ -1,9 +1,19 @@
 output "topic_id" {
-  description = "ID completo del topic Pub/Sub creato (projects/PROJECT_ID/topics/TOPIC_NAME)."
-  value       = google_pubsub_topic.main_topic.id # .id fornisce il nome completo
+  description = "Full ID of the main Pub/Sub topic created (projects/PROJECT_ID/topics/TOPIC_NAME)."
+  value       = google_pubsub_topic.main_topic.id
 }
 
 output "topic_name" {
-  description = "Nome breve del topic Pub/Sub creato."
+  description = "Short name of the main Pub/Sub topic created."
   value       = google_pubsub_topic.main_topic.name
+}
+
+output "dlq_topic_id" {
+  description = "Full ID of the Dead-Letter Queue (DLQ) Pub/Sub topic created."
+  value       = google_pubsub_topic.dlq_topic.id
+}
+
+output "dlq_topic_name" {
+  description = "Short name of the Dead-Letter Queue (DLQ) Pub/Sub topic created."
+  value       = google_pubsub_topic.dlq_topic.name
 }
